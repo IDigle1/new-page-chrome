6a3d60e8dccf4f29012192ce60fb168718688ccd
Evgenii Falaleev <falaleev@arcsinus.com>
2024-09-11 18:19:34 +0300

    fix

--- src/widgets/Widgets/ui/Widgets.vue
+++ src/widgets/Widgets/ui/Widgets.vue
@@ -16,13 +16,13 @@ const widgets = {
 
 const widgetStore = useWidgetsStore()
 
-const move = (e, widgetName: string) => {
-    return (we) => {
+const move = (e: any, widgetName: string) => {
+    return (we: any) => {
         widgetStore.changeWidgetPosition(widgetName, we.screenX - e.offsetX, we.screenY - e.offsetY - 205)
     }
 }
 
-const mousedownHanler = (e: MouseEvent, widgetName: string) => {
+const mousedownHanler = (e: MouseEvent, widgetName: keyof typeof widgets) => {
     widgets[widgetName].moveHandler = move(e, widgetName)
     window.addEventListener('mousemove', widgets[widgetName].moveHandler)
     window.addEventListener('mouseup', () => {
@@ -37,7 +37,7 @@ const mousedownHanler = (e: MouseEvent, widgetName: string) => {
     <component
         v-for="w in widgetStore.widgets"
         :ref="w.name"
-        :is="widgets[w.name].component"
+        :is="widgets[w.name as keyof typeof widgets].component"
         :key="w.name"
         :class="`fixed`"
         :style="`left: ${w.posX}px; top: ${w.posY}px`"
